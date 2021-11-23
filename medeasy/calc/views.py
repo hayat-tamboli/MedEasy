4f73cceb98271861820fd3fb72936e78498f3c69
Ashutosh <ashutosh.j001@gmail.com>
2021-11-23 22:06:52 +0530

    name change

--- medeasy/calc/views.py
+++ medeasy/calc/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html',{'name':"Ashutosh" , 'class':'13'})
+    return render(request, 'home.html',{'name':"Hayat" , 'class':'13'})
